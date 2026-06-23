06fbed1389710cdaa8d3699e1375b9c1ce4ce761
Faheem7676 <farooqui.faheem033@gmail.com>
2026-06-23 11:25:51 +0530

    Improve navbar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,15 @@ function Navbar() {
   return (
     <nav>
       <h2>Shama Dental Clinic</h2>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Testimonials</li>
+        <li>Contact</li>
+      </ul>
+
+      <button>Book an Appointment</button>
     </nav>
   );
 }
